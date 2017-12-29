5fdc0a8ee928eced5d5e9088512059244a095273
최윤호 <choiking10@naver.com>
2017-12-29 17:13:34 +0900

    add too handler

--- YHbot/handler.py
+++ YHbot/handler.py
@@ -2,7 +2,7 @@ import random
 import copy
 
 from crypto_games.handler import CryptoHandler
-from crypto_games.bet import Betting
+from crypto_games.betting_info import Betting
 
 
 class MyHandler(CryptoHandler):
@@ -37,3 +37,58 @@ class MyHandler(CryptoHandler):
 
     def after_bet(self, bet_result, budget):
         pass
+
+
+class KooHandler(CryptoHandler):
+    def __init__(self, coin_kind, base_rate=0.001, base_payout=1.1, base_pump=10,
+                 seed_money=None,
+                 priority=10000, probability=0.5):
+        super(KooHandler, self).__init__(priority=priority,
+                                         probability=probability)
+        self.coin_kind = coin_kind
+        self.base_rate = base_rate
+        self.base_payout = base_payout
+        self.base_pump = base_pump
+        self.seed_money = None
+        self.accum = 0
+
+    def place_base_bet(self, place_money, under_over):
+        return Betting(
+            payout=self.base_payout,
+            under_over=under_over,
+            bet=place_money,
+            coin_kind=self.coin_kind,
+            additional=KooHandler
+        )
+
+    def execution_probability(self, bet_log):
+        if len(bet_log) < 3:
+            return 0
+
+        return super(KooHandler, self).execution_probability(bet_log)
+
+    def place_bet(self, bet_log, budget):
+        check_under = 0
+        check_over = 0
+        if self.seed_money is None:
+            self.seed_money=budget
+
+        for i in range(0, 3):
+            if 5 <= bet_log[i].roll <= 45:
+                check_under += 1
+            else:
+                check_under -= 2
+
+            if 60 <= bet_log[i].roll <= 95:
+                check_over += 1
+            else:
+                check_over -= 2
+
+        place_money = self.seed_money * self.base_rate * \
+                      (self.base_pump ** self.accum)
+        if check_under >= 2:
+            return self.place_base_bet(place_money, True)
+        if check_over >= 2:
+            return self.place_base_bet(place_money, False)
+
+        return None
--- YHbot/tests.py
+++ YHbot/tests.py
@@ -1,18 +1,18 @@
 
 from crypto_games.looper import Looper
 from crypto_games.betting_info import Betting
-from YHbot.handler import MyHandler
+from YHbot.handler import MyHandler, KooHandler
 
 default = Betting(coin_kind="BTC",
                   bet=0.00000001,
-                  payout=2,
+                  payout=1.15,
                   under_over=False,
                   client_seed=None)
 
 while True:
     mylooper = Looper("secret",
-                      default, 0.00000640)
-    mylooper.add_handler(MyHandler())
+                      default, 0.00001000)
+    mylooper.add_handler(KooHandler(coin_kind="BTC", base_rate=0.01))
     try:
         mylooper.run()
     except BaseException as e:
